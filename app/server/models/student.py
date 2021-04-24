5f93d4dc43322771992d7b4f23da98668022c15c
Kristian Jackson <kristian.jackson@gmail.com>
2021-04-24 13:00:38 -0400

    Update student.py

--- app/server/models/student.py
+++ app/server/models/student.py
@@ -4,7 +4,7 @@ class StudentSchema(BaseModel):
     fullname: str = Field(...)
     email: EmailStr = Field(...)
     course_of_study: str = Field(...)
-    year: int = Field(...), gt-0, lt=9)
+    year: int = Field(...), gt=0, lt=9)
     gpa: float = Field(...), le=4.0)
 
     class Config:
