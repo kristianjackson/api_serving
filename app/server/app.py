8267cb32e552893c35ca5b0ffda922dc45e8fb5a
Kristian Jackson <kristian.jackson@gmail.com>
2021-04-27 11:02:58 -0400

    Update app.py

--- app/server/app.py
+++ app/server/app.py
@@ -5,8 +5,17 @@ from app.server.routes.student import router as StudentRouter
 app = FastAPI()
 
 app.include_router(StudentRouter, tags=["Student"], prefix='/student')
-app.include_router(DiamondRouter, tags=["Diamond"], prefix='/diamond')
 
 @app.get("/", tags=["Root"])
 async def read_root():
     return{"message": "Welcome to this fantastic app!"}
+
+@app.post('/diamond')
+def predict_diamond(caret_weight, cut, color, clarity, polish, symmetry, report):
+    data = pd.Dataframe([[caret_weight, cut, color, clarity, polish, symmetry, report]])
+    data.columns = ['Caret Weight', 'Cut', 'Color', 'Clarity', 'Polish', 'Symmetry', 'Report']
+    
+    model = load_model('diamond_pipeline')
+    
+    predictions = predict_model(model, data)
+    return {'prediction': int(predictions['Label'][0])}
